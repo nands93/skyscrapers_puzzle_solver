eb78aa10cf3573eee60e1811038ab48e51e72b7a
nands93 <femmarques93+github@gmail.com>
2023-07-11 17:09:14 -0300

    final

--- ex00/rush01.c
+++ ex00/rush01.c
@@ -6,7 +6,7 @@
 /*   By: femarque <femarque@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 13:57:31 by femarque          #+#    #+#             */
-/*   Updated: 2023/07/11 16:08:43 by femarque         ###   ########.fr       */
+/*   Updated: 2023/07/11 17:08:38 by femarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,18 @@ int	array_len(char **array)
 
 int	main(int argc, char **argv)
 {
-	int	res[16];
-	int	grid[SIZE][SIZE];
-	char **grids;
-	int	i;
-	int len;
-	
+	int		res[16];
+	int		grid[SIZE][SIZE];
+	char	**grids;
+	int		i;
+	int		len;
+
 	ft_memset(grid, 0, sizeof(grid));
 	grids = ft_split(argv[1], ' ');
 	len = array_len(grids);
 	if (argc != 2)
 	{
-		ft_error("Error 1");
+		ft_error("Error");
 		return (1);
 	}
 	i = -1;
@@ -44,7 +44,6 @@ int	main(int argc, char **argv)
 	if (fill_grid(0, 0, res, grid))
 		print_grid(grid);
 	else
-		ft_error("Error 2");
+		ft_error("Error");
 	return (0);
 }
-
